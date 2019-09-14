c5c614b6c57de8443f11d807bad46b1b53c5b896
Дмитрий Андреев <djandreev89@mail.ru>
2019-09-14 13:43:41 +1000

    Добавьте файлы проекта.

---
+++ workWithText/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace workWithText
+{
+    class Program
+    {
+        struct Word
+        {
+            public int indexOf;
+            public int lenght;
+            public int colour;
+        }
+        static void Main(string[] args)
+        {
+            string text = @"В нашей команде есть программист   Иван.
+Иван умеет разрабатывать мобильные и веб приложения.
+Каждый программист в нашей команде программирует на языке С#.
+Однако Иван знает ещё язык PHP....";
+
+
+
+
+
+            string[] arrText = text.Split(new char[] { ' ', ',', '.', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+            List<string> lDublicate = new List<string>();
+
+            List<Word> dublicateList = new List<Word>();
+
+
+            for (int i = 0; i < arrText.Length - 1; i++)
+            {
+                for (int k = i + 1; k < arrText.Length; k++)
+                {
+                    if (arrText[i] == arrText[k] && !lDublicate.Contains(arrText[i]))
+                    {
+                        lDublicate.Add(arrText[i]);
+                    }
+                }
+            }
+
+            for (int i = 0; i < lDublicate.Count; i++)
+            {
+                int findIndex = 0;
+                while (findIndex != -1)
+                {
+                    findIndex = text.IndexOf(lDublicate[i], findIndex);
+                    if (findIndex > -1)
+                    {
+                        dublicateList.Add(new Word()
+                        {
+                            indexOf = findIndex,
+                            lenght = lDublicate[i].Length,
+                            colour = i + 1
+                        });
+                        findIndex += 1;
+                    }
+                }
+            }
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                for (int l = 0; l < dublicateList.Count; l++)
+                {
+                    if (i == dublicateList[l].indexOf)
+                    {
+                        ConsoleColor myColor = (ConsoleColor)dublicateList[l].colour;
+                        Console.ForegroundColor = myColor;
+                        for (int m = i; m < dublicateList[l].lenght + i; m++)
+                        {
+                            Console.Write(text[m]);
+                        }
+                        i += dublicateList[l].lenght;
+                        Console.ResetColor();
+                        continue;
+                    }
+                }
+
+                Console.Write(text[i]);
+            }
+
+            //for (int i = 0; i < arrText.Length; i++)
+            //{
+            //    Console.ResetColor();
+            //    for (int k = 0; k < lDublicate.Count; k++)
+            //    {
+            //        if (arrText[i] == lDublicate[k])
+            //        {
+            //            ConsoleColor myColor = (ConsoleColor)k + 3;
+            //            Console.ForegroundColor = myColor;
+            //        }
+            //    }
+            //    Console.Write(arrText[i] + " ");
+            //}
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ workWithText/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("workWithText")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("MICROSOFT")]
+[assembly: AssemblyProduct("workWithText")]
+[assembly: AssemblyCopyright("Copyright © MICROSOFT 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("c3a8dfc8-f081-46d7-8e39-191863d10385")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
